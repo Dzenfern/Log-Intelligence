26c764fa9b5cf7e7ce6e68c2d31505481116eee4
Dylan <dylanzfernandes@gmail.com>
2026-05-03 15:32:28 +0530

    Started project and added dependencies

---
+++ src/test/java/com/dylan/LogIntelligence/LogIntelligenceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylan.LogIntelligence;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogIntelligenceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
