e8e8ffe0bd1603311f3172dab7c5d9f911008cdb
Dylan <dylanzfernandes@gmail.com>
2026-05-03 15:35:42 +0530

    Started project and added dependencies

---
+++ src/main/java/com/dylan/logintelligence/LogIntelligenceApplication.java
@@ -0,0 +1,13 @@
+package com.dylan.logintelligence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LogIntelligenceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LogIntelligenceApplication.class, args);
+	}
+
+}
